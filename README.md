3d67a0dad43e1a0390061b972a27cb6656c809ca
william <wiliancosta51@hotmail.com>
2023-09-10 16:40:25 -0300

    feat: test's and readme increment's

--- README.md
+++ README.md
@@ -1,21 +1,58 @@
-## Instale as depêndencias
+# In this project, you can update the information of one or more products, but in order to do so, you must comply with some business rules, such as:
+
+<div>
+<p>
+  1. The new product value cannot be lower than its manufacturing cost.
+</p>
+<p>
+  2. The discount value should not exceed 10%, either more or less.
+</p>
+<p>
+  3. The product should only be updated if the frontend sends "true" in the request.
+</p>
+</div>
+
+## Used in this project:
+
+<img src="https://img.shields.io/badge/node js%20-%2320232a.svg?&style=for-the-badge&color=339933&logo=node.js&logoColor=ffffff"/>
+<img src="https://img.shields.io/badge/nestjs-E0234E?style=for-the-badge&logo=nestjs&logoColor=white" />
+<img src="https://img.shields.io/badge/TypeScript%20-%2320232a.svg?&style=for-the-badge&color=3178C6&logo=TypeScript&logoColor=ffffff"/>
+<img src="https://img.shields.io/badge/Prisma%20-%2320232a.svg?&style=for-the-badge&color=4C51BF&logo=Prisma&logoColor=ffffff"/>
+<img src="https://img.shields.io/badge/jest%20-%2320232a.svg?&style=for-the-badge&color=C21325&logo=jest&logoColor=ffffff"/>
+<img src="https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white"/>
+
+## Install Dependencies
 
 ```bash
 $ npm install
 ```
 
-## Adicione a variável de ambiente
+## Add a environment variable
 
-Exemplo: DATABASE_URL="mysql://user:password@localhost:3306/mydb?schema=public"
+#### Exemple: DATABASE_URL="mysql://user:password@localhost:3306/mydb?schema=public"
 
-## Crie o banco com o Prisma
+## Create database with Prisma
 
 ```
 $ npx prisma migrate dev
 ```
 
-## Rode a aplicação
+## Run the application
 
 ```
 $ npm run start:dev
+```
+
+## To run e2e tests
+
+### Create a test environment:
+
+#### Exemple: .env.test
+
+#### Exemple URL: DATABASE_URL="mysql://user:password@localhost:3306/mydb_test?schema=public"
+
+## Run the test
+
+```
+  $ npm run test:e2e
 ```
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,9 +5,10 @@ import { AppModule } from './../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { PrismaModule } from '../src/prisma/prisma.module';
 
-describe('AppController (e2e)', () => {
+describe('Health test', () => {
   let app: INestApplication;
   let prisma: PrismaService;
+  const URL = '/products/csv';
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -18,6 +19,14 @@ describe('AppController (e2e)', () => {
     app.useGlobalPipes(new ValidationPipe());
     await prisma.pack.deleteMany();
     await prisma.product.deleteMany();
+    await prisma.product.create({
+      data: {
+        code: 1,
+        costPrice: 15,
+        name: 'Rice',
+        salesPrice: 20,
+      },
+    });
     await app.init();
   });
   it('GET /health ', () => {
@@ -25,5 +34,54 @@ describe('AppController (e2e)', () => {
       .get('/health')
       .expect(200)
       .expect("I'm okay!");
+  });
+  describe('Products test POST /products/csv', () => {
+    it('Should return status 400 Bad Request if no product_code is given', async () => {
+      const { status } = await request(app.getHttpServer()).post(URL).send({
+        boolean: true,
+        new_price: '12.50',
+      });
+      expect(status).toBe(HttpStatus.BAD_REQUEST);
+    });
+    it('Should return status 400 Bad Reques if no new_price is given', async () => {
+      const { status } = await request(app.getHttpServer()).post(URL).send({
+        boolean: true,
+        product_code: '12',
+      });
+      expect(status).toBe(HttpStatus.BAD_REQUEST);
+    });
+    it('Should return status 404 when product_code does not exist', async () => {
+      const { status, body } = await request(app.getHttpServer())
+        .post(URL)
+        .send({
+          boolean: false,
+          product_code: '25',
+          new_price: '28',
+        });
+      expect(status).toBe(HttpStatus.NOT_FOUND);
+      expect(body.message).toBe('Product not found');
+    });
+    it('Should not update a price if the boolean is false, and give a body with product info', async () => {
+      const { body } = await request(app.getHttpServer()).post(URL).send({
+        boolean: false,
+        product_code: '1',
+        new_price: '25',
+      });
+      expect(body).toStrictEqual({
+        name: 'Rice',
+        currentPrice: 20,
+        newPrice: 25,
+        code: 1,
+        percentage: -25,
+      });
+    });
+    it('Should return status 201 and update a price if boolean is true', async () => {
+      const { status } = await request(app.getHttpServer()).post(URL).send({
+        boolean: true,
+        product_code: '1',
+        new_price: '22',
+      });
+      expect(status).toBe(HttpStatus.CREATED);
+    });
   });
 });
